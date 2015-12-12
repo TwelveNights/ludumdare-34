2584bbc943791bfbf4d69f35b3c10525d1e0e80d
LuckiiGoggy <luck-13@live.ca>
2015-12-12 04:13:46 -0800

    Moving Robot to different Point Of Interest Done

---
+++ My,Robot/Assets/Script/PointOfInterest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointOfInterest : MonoBehaviour {
+    [SerializeField]
+    private Transform m_PlayerObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+	}
+
+    void OnMouseDown()
+    {
+        m_PlayerObject.transform.position = transform.position;
+
+    }
+}
