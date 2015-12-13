8240578729dfea348762f9f9e8afeb536732e506
LuckiiGoggy <luck-13@live.ca>
2015-12-12 17:03:38 -0800

    Opening UI Menu when workshop is clicked

--- My,Robot/Assets/Script/ModuleAction.cs
+++ My,Robot/Assets/Script/ModuleAction.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Script
 {
-    class ModuleAction
+    /// <summary>
+    /// Module Action, when clicked should trigger the UI
+    /// system for adding, modifying, selecting, etc of 
+    /// Modules for the robot.
+    /// </summary>
+    public class ModuleAction : POIAction
     {
+        public GameObject UIModule;
+
+
+        void Start()
+        {
+            UIModule.SetActive(false);
+        }
+
+        public override void DoAction()
+        {
+            UIModule.SetActive(true);
+           // UIModule.GetComponent<Text
+            
+            base.DoAction();
+        }
     }
 }
