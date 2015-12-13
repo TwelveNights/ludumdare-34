c3d8007ee70a6e50919518d1f56b9ce5cd624aa0
LuckiiGoggy <luck-13@live.ca>
2015-12-13 00:08:01 -0800

    Bring Back the Meta

--- My,Robot/Assets/Script/Module/UI/ModuleMenu.cs
+++ My,Robot/Assets/Script/Module/UI/ModuleMenu.cs
@@ -10,23 +10,29 @@ namespace Assets.Script.Module.UI
     {
         List<Module> availableModules;
 
-        public GameObject UIModel;
+        public Transform UIModel;
 
         public float a;
 
         void Start()
         {
             availableModules = new List<Module>();
-            GameObject test = Instantiate(UIModel, transform.position + Vector3.down * a, Quaternion.identity) as GameObject;
 
-            test.transform.SetParent(transform);
+            for(int i = 0; i < 3; i++)
+            {
+                GameObject obj = Instantiate(UIModel, transform.position, Quaternion.identity) as GameObject; 
+                RectTransform rect = obj.GetComponent<RectTransform>();
+                rect.position = transform.position + Vector3.down * a + Vector3.down * rect.rect.height * i * 2;
+
+                obj.transform.SetParent(transform);
+            }
         }
 
         void Update()
         {
             foreach(Transform a in transform)
             {
-                DestroyObject(a.GetComponent<GameObject>());
+                //DestroyObject(a.GetComponent<GameObject>());
             }
         }
 
