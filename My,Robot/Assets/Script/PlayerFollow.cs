96ea70d1f1041afb4dff52ebb825dc5e22bf115e
LuckiiGoggy <luck-13@live.ca>
2015-12-14 13:02:21 -0800

    Combined GameScene and GameSceneG Added Lily Image for Health HUD

---
+++ My,Robot/Assets/Script/PlayerFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Script;
+
+public class PlayerFollow : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 pos = GameInfo.player.transform.position;
+
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+
+	}
+}
